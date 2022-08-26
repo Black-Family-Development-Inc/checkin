5caa82c4cf9b36d8c8e79e8c95c3efa3ac612e75
Erick Bravo <67840199+Erick-Bravo@users.noreply.github.com>
2022-08-26 12:38:53 -0700

    landing page graphql data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,24 @@
+import { graphql, PageProps } from "gatsby";
 import * as React from "react";
 import Accordion from "../components/Accordion/Accordion";
-import Button from "../components/Button/Button";
+// import Button from "../components/Button/Button";
 import DefaultLayout from "../layouts/DefaultLayout/DefaultLayout";
 
-const IndexPage = () => {
+const IndexPage = ({ data }: PageProps<Queries.IndexPageQuery>) => {
+  const header = data.contentfulPage?.header;
+  const body = data.contentfulPage?.body?.body;
+  const buttonData: any = data.contentfulPage?.assessmentButton;
+
+  const handleClick = () => {
+    window.open(buttonData.link, "_blank", "noopener,noreferrer"); //opens button link in a new tab
+  };
+
   return (
     <>
       <DefaultLayout>
-        <Button />
+        <h1>{header}</h1>
+        <p>{body}</p>
+        <button onClick={handleClick}>{buttonData?.text}</button>
         <Accordion />
       </DefaultLayout>
     </>
@@ -15,3 +26,19 @@ const IndexPage = () => {
 };
 
 export default IndexPage;
+
+export const query = graphql`
+  query IndexPage {
+    contentfulPage(title: { eq: "Home Page" }) {
+      title
+      header
+      body {
+        body
+      }
+      assessmentButton {
+        link
+        text
+      }
+    }
+  }
+`;
