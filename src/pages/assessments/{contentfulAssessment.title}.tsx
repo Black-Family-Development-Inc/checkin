27b1ef7f54efe22f9cf81e7c374d53c273e1b0ed
Brandon Benefield <brandon.benefield@detroitlabs.com>
2022-09-07 12:33:33 -0400

    Display Prev and Next buttons that can paginate through an array of questions

--- src/pages/assessments/{contentfulAssessment.title}.tsx
+++ src/pages/assessments/{contentfulAssessment.title}.tsx
@@ -1,23 +1,49 @@
+import { Button } from "@mui/material";
 import { graphql, PageProps } from "gatsby";
-import React from "react";
+import React, { useState } from "react";
+
+const questions = ["This", "Is", "Just", "Filler", "Data", "🐱"];
 
 const AssessmentPage = ({ data }: PageProps<Queries.AssessmentPageQuery>) => {
   const { contentfulAssessment: assessment } = data;
+  const [currentQuestionIdx, setCurrentQuestionIdx] = useState<number>(0);
+
+  const clamp = (num: number) =>
+    Math.min(Math.max(num, 0), questions.length - 1);
+
   return (
     <>
       <p>Assessment ID: {assessment?.id}</p>
       <p>Assessment Title: {assessment?.title}</p>
       <p>Assessment "Questions":</p>
+      <p>
+        You are on question {currentQuestionIdx + 1} out of {questions.length}
+      </p>
       <ul>
-        {assessment?.repeaterList?.map((el) => {
-          return (
-            <div key={el?.id}>
-              <li>{el?.key}</li>
-              <li>{el?.value}</li>
-            </div>
-          );
-        })}
+        <li>one</li>
+        <li>two</li>
       </ul>
+
+      <div>
+        <Button
+          onClick={() => setCurrentQuestionIdx(clamp(currentQuestionIdx - 1))}
+          disabled={currentQuestionIdx === 0}
+        >
+          Previous
+        </Button>
+
+        {currentQuestionIdx === questions.length - 1 ? (
+          <Button onClick={() => alert(questions[questions.length - 1])}>
+            Results
+          </Button>
+        ) : (
+          <Button
+            onClick={() => setCurrentQuestionIdx(clamp(currentQuestionIdx + 1))}
+          >
+            Next
+          </Button>
+        )}
+      </div>
     </>
   );
 };
@@ -29,11 +55,6 @@ export const query = graphql`
     contentfulAssessment(title: { eq: $title }) {
       id
       title
-      repeaterList {
-        id
-        key
-        value
-      }
     }
   }
 `;
