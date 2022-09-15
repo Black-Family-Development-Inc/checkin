26954d4ecba926c9d507c1d886ddf4880bad2b04
dbrown-dl <donovan.brown@detroitlabs.com>
2022-09-15 12:34:39 -0400

    add comments to remove temp varibles and Accordion component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,23 @@
 import { graphql, PageProps } from "gatsby";
 import * as React from "react";
+import Accordion from "../components/Accordion/Accordion";
 import { FirstSection } from "../components/pages/IndexPage/FirstSection";
 import DefaultLayout from "../layouts/DefaultLayout/DefaultLayout";
 import { IndexPageTypes } from "./IndexPage-types";
 
 const IndexPage = ({ data }: PageProps<IndexPageTypes>) => {
+  // Temp data variables to test component props, please remove once accordion component is removed
+  const tempAccordionTitle =
+    "This is data prop for the header of the Accordion";
+  const tempAccordionBody =
+    "And this is data prop for the body of the accordion";
+
   return (
     <>
       <DefaultLayout>
         <FirstSection {...data.contentfulHomePage} />
+        {/* Please remove Accordion once FAQ paged is created */}
+        <Accordion title={tempAccordionTitle} body={tempAccordionBody} />
       </DefaultLayout>
     </>
   );
