dfe834807d29dfdf38c8086fa6f8c674ab52e84b
Brandon Benefield <brandon.benefield@detroitlabs.com>
2022-09-26 14:21:45 -0400

    Make new results-section route for testing in isolution

---
+++ src/components/pages/ResultsPage/ResultsSection/ResultsSection.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ResultsSection = () => {
+  return <h1>Results Section</h1>;
+};
+
+export default ResultsSection;
---
+++ src/pages/components/results/results-section.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ResultsSectionPage = () => {
+  return <h1>Results Section</h1>;
+};
+
+export default ResultsSectionPage;
