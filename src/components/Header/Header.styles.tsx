65d76f146ecfe24d5c098c4475104ab9aedd2b9f
Brandon Benefield <brandon.benefield@detroitlabs.com>
2022-10-03 10:57:44 -0400

    Add styles to FAQ page

--- src/components/Header/Header.styles.tsx
+++ src/components/Header/Header.styles.tsx
@@ -3,4 +3,6 @@ import { Typography } from "@mui/material";
 
 export const HeaderStyled = styled(Typography)({
   fontSize: "1.5rem",
+  fontWeight: 600,
+  marginBottom: 8,
 });
---
+++ src/components/Paragraph/Paragraph.styles.tsx
@@ -0,0 +1,7 @@
+import styled from "@emotion/styled";
+import { Typography } from "@mui/material";
+
+export const TypographyStyled = styled(Typography)({
+  marginTop: 12,
+  marginBottom: 36,
+});
