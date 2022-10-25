8c1e30be40e63058801ee8c2d9905eb454b71bd4
Brandon Benefield <brandon.benefield@detroitlabs.com>
2022-10-25 18:25:34 -0400

    Change <Paragraph /> fontSize at breakpoints of tablet and higher

--- src/components/Paragraph/Paragraph.styles.tsx
+++ src/components/Paragraph/Paragraph.styles.tsx
@@ -1,9 +1,14 @@
 import styled from "@emotion/styled";
 import { Typography } from "@mui/material";
+import { color } from "../../styles/colors";
 
 export const TypographyStyled = styled(Typography)({
+  color: color.gray.one,
   fontFamily: "Roboto",
   fontSize: "16px",
   lineHeight: "19px",
   letterSpacing: "0.15px",
+  "@media(min-width: 768px)": {
+    fontSize: 18,
+  },
 });
