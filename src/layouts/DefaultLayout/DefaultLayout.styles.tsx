d96b553e610d389ebd3017a5d2a5922a07a2fa1a
Brandon Benefield <brandon.benefield@detroitlabs.com>
2022-11-11 13:05:40 -0500

    DefaultLayout now takes new prop "isAssessmentPage" to determine topPadding

--- src/layouts/DefaultLayout/DefaultLayout.styles.tsx
+++ src/layouts/DefaultLayout/DefaultLayout.styles.tsx
@@ -1,20 +1,26 @@
 import styled from "@emotion/styled";
 import { breakpoints, color, maxWidths } from "../../styles/theme";
+import { LayoutContainerWrapperTypes } from "./DefaultLayout-types.d";
 
-export const LayoutContainerWrapper = styled.div({
-  display: "flex",
-  flexDirection: "column",
-  alignItems: "center",
-  padding: "80px 16px 0px",
-  background: color.teal.five,
+export const LayoutContainerWrapper = styled.div(
+  (props: LayoutContainerWrapperTypes) => ({
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    paddingTop: props.isAssessmentLayout ? 40 : 80,
+    paddingLeft: 16,
+    paddingRight: 16,
+    paddingBottom: 0,
+    background: color.teal.five,
 
-  [`@media(min-width: ${breakpoints.tablet}px)`]: {
-    padding: "88px 112px 0px",
-  },
-  [`@media(min-width: ${breakpoints.desktop}px)`]: {
-    padding: "104px 162px 0px",
-  },
-});
+    [`@media(min-width: ${breakpoints.tablet}px)`]: {
+      padding: "88px 112px 0px",
+    },
+    [`@media(min-width: ${breakpoints.desktop}px)`]: {
+      padding: "104px 162px 0px",
+    },
+  }),
+);
 
 export const LayoutContainerStyled = styled("div")({
   background: color.teal.five,
