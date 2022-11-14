c8f03aba5a736c2a9d0e087ea8499648f42979b9
Erick Bravo <67840199+Erick-Bravo@users.noreply.github.com>
2022-11-14 08:39:11 -0800

    hardocoded footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,8 +1,20 @@
+import { Link, Typography } from "@mui/material";
 import React from "react";
 import { FooterStyled } from "./Footer.styles";
 
 const Footer = () => {
-  return <FooterStyled>FOOTER COMPONENT</FooterStyled>;
+  return (
+    <FooterStyled>
+      <Typography>HOPE IS HERE!</Typography>
+      <Typography>Main Office: 2995 E Grand BLVD, Detroit, MI 48202</Typography>
+      <Typography>
+        Call BFDI for support: 1-877-SEE-HOPE (844-733-4673)
+      </Typography>
+      <Link href="https://www.blackfamilydevelopment.org/">
+        BFDI Main Website
+      </Link>
+    </FooterStyled>
+  );
 };
 
 export default Footer;
