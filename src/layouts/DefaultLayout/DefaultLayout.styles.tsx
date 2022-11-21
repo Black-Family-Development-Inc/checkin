ea835cbb0085ce3ef5389b386aef7c448a02e859
James York <kroysemaj@gmail.com>
2022-11-21 16:51:57 -0500

    Add styles for universal check in page

--- src/layouts/DefaultLayout/DefaultLayout.styles.tsx
+++ src/layouts/DefaultLayout/DefaultLayout.styles.tsx
@@ -7,12 +7,24 @@ export const LayoutContainerWrapper = styled.div({
   alignItems: "center",
   padding: "80px 16px 0px",
   background: color.teal.five,
-
+  "&.assessment": {
+    padding: "0 16px 80px",
+  },
+  ".button-container": {
+    width: maxWidths.mobile.buttons,
+    margin: "0 auto",
+  },
   [`@media(min-width: ${breakpoints.tablet}px)`]: {
     padding: "88px 112px 0px",
+    ".button-container": {
+      width: maxWidths.tablet.buttons,
+    },
   },
   [`@media(min-width: ${breakpoints.desktop}px)`]: {
     padding: "104px 162px 0px",
+    ".button-container": {
+      width: maxWidths.desktop.buttons,
+    },
   },
 });
 
