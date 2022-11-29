fefea4d8140770bdf7c7af1724ea7ec0e159ad0b
Erick Bravo <67840199+Erick-Bravo@users.noreply.github.com>
2022-11-29 15:26:30 -0800

    Other resources section styled w/ breakpoints

--- src/components/pages/ResultsPage/OtherResources/OtherResources.tsx
+++ src/components/pages/ResultsPage/OtherResources/OtherResources.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Header } from "../../../Header";
 import { Paragraph } from "../../../Paragraph";
+import { OtherResourcesContainer } from "./OtherResources.styles";
 import { OtherResourcesTypes } from "./OtherResources-types";
 
 const OtherResources = ({
@@ -8,12 +9,12 @@ const OtherResources = ({
   otherResourcesDescription,
 }: OtherResourcesTypes) => {
   return (
-    <>
+    <OtherResourcesContainer>
       <Header variant="h2" text={otherResourcesHeader} />
       <Paragraph sx={{ margin: "12px 0px 36px" }}>
         {otherResourcesDescription.raw}
       </Paragraph>
-    </>
+    </OtherResourcesContainer>
   );
 };
 
