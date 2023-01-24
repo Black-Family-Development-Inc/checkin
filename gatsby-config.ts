d7dcf12c14e60b13e608b245a1e686e475deca5a
Ryan Donnelly <ryan.donnelly@detroitlabs.com>
2023-01-24 13:38:38 -0500

    add path prefix for pages deployment

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -7,6 +7,7 @@ const config: GatsbyConfig = {
     title: `BFDI`,
     siteUrl: process.env.SITE_NAME,
   },
+  pathPrefix: "/BFDI",
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
