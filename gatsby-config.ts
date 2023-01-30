1c4ba8163944e344e3f26056222145d0cd1c18ea
Ryan Donnelly <ryan.donnelly@detroitlabs.com>
2023-01-30 11:45:01 -0500

    add temporary path prefix, remove when moving to subdomain

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -7,6 +7,8 @@ const config: GatsbyConfig = {
     title: `BFDI`,
     siteUrl: process.env.SITE_NAME,
   },
+  // add temporary path prefix, remove when moving to subdomain
+  pathPrefix: "/hopeishere",
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
